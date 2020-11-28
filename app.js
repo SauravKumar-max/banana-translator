3ad2188a42bdee30ca69bd13199453e46abf210e
saurav-max <sauravmax237@gmail.com>
2020-11-28 20:24:27 +0530

    added event listener

---
+++ app.js
@@ -0,0 +1,7 @@
+var text = document.querySelector('.txt-translate');
+var btn = document.querySelector('.btn-translate');
+var translatedText = document.querySelector(".txt-translated");
+
+
+
+btn.addEventListener('click', translation);
