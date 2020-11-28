4fbaa0c807e5da0936090cc7f3a903ffc1ffc718
saurav-max <sauravmax237@gmail.com>
2020-11-28 20:27:57 +0530

    feat: added translation

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ var text = document.querySelector('.txt-translate');
 var btn = document.querySelector('.btn-translate');
 var translatedText = document.querySelector(".txt-translated");
 
+function errorHandler(error){
+    console.log("error:", error);
+    alert("something went wrong! try again");
+}
 
+function translation() {
+    var url = "https://api.funtranslations.com/translate/minion.json?text=" + text.value;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+    
+          var minionText = json.contents.translated;
+          translatedText.innerText = minionText;
+    })
+    .catch(errorHandler);
+}
 
 btn.addEventListener('click', translation);
